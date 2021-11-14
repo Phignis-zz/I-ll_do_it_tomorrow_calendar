e77351bf54a086e941a2e7d7b1363546b34b9394
Phignis <baptiste63.foucras.pro@gmail.com>
2021-11-14 20:30:36 +0100

    ajout de divers dossiers, dont nottament ressources (partie html, images, css...), début du code du header

---
+++ src/calendar_site/ressources/html_parts/header.php
@@ -0,0 +1,15 @@
+<!--    Le but de ce fichier est de proposer un header a inclure a chaque début de page html du site "I'll Do It Tomorrow Calendar"
+        Il n'a pas sens à être appelé seul, expliquant l'absance des balises html -->
+        
+<header>
+        <?php
+                // intégration de la partie d'affichage du profil connecté, ou l'option de connexion
+                if(!isset($_SESSION("pseudo"))) {
+                        require();
+                }
+                else {
+                        require();
+                }
+                
+        ?> 
+</header>
