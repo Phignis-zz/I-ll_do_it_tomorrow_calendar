be1a4355d75ccd33a95c39c82fc8201299137c14
Phignis <baptiste63.foucras.pro@gmail.com>
2021-11-21 17:23:46 +0100

    créatuon du footer et d'un début de config, moove des objets aux bons dossiers

--- src/ressources/html_parts/header.php
+++ src/ressources/html_parts/header.php
@@ -1,5 +1,8 @@
-<!--    Le but de ce fichier est de proposer un header a inclure a chaque début de page html du site "I'll Do It Tomorrow Calendar"
-        Il n'a pas sens à être appelé seul, expliquant l'absance des balises html -->
+
+<!--
+        Le but de ce fichier est de proposer un header a inclure a chaque début de page html du site "I'll Do It Tomorrow Calendar"
+        Il n'a pas sens à être appelé seul, expliquant l'absance des balises html
+-->
         
 <header>
         <?php
