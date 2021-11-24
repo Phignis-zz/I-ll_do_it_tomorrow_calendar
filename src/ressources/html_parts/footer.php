05fe39f503e481eebf2bde06e0684b26444508d6
Mickael DUBOIS <midubois1@IUTCLINFA2319.iut.local>
2021-11-24 15:30:59 +0100

    ajout de connexion et du footer

--- src/ressources/html_parts/footer.php
+++ src/ressources/html_parts/footer.php
@@ -4,29 +4,28 @@
         Il n'a pas pour but d'être appelé seul, expliquant l'absance de balises html, head ou body
 -->
 
-<footer>
-        <div class="d-flex justify-content-around" id="footer_flex_div">
-                <p id="nom_page_paragraphe">
-                        <?php
-                                $ROOT_PATH = "C:/Baptiste/IUT/2A_IUT/projets/php_projet/git_php_projet";
-                        
-                                // la variable $nomPage contient normalement le nom de la page censée être display
-                                if(!isset($nomPage) || empty($nomPage)) $nomPage = "Unknown";
-                                echo $nomPage;
-                        ?>
-                </p>
-                <img src="<?php echo $ROOT_PATH . "/src/ressources/images/logo.png";?>" alt="logo application.png">
-                
-                <ul class="nav flex-column">
-                        <li class="nav-item mb-2">
-                                DUBOIS Mickaël
-                        </li>
-                        <li class="nav-item mb-2">
-                                FOUCRAS Baptiste
-                        </li>
-                        <li class="nav-item mb-2">
-                                The I'll do it tomorrow calendar
-                        </li>
-                </ul>
-        </div>
-</footer>
+<!doctype html>
+<html lang="fr">
+        <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            
+        <link rel="stylesheet" href="../ressources/style/footer.css">
+
+        <title>Procrastinator Calendar</title>
+        </head>
+        <footer>
+                <div>
+                       <img src="../ressources/images/logo.png" alt="o">
+                </div>
+                <div>
+                        <p>- Anonymous -</p>
+                </div>
+                <div id="credits">
+                        <p>By:</p>
+                        <p>FOUCRAS Baptiste</p>
+                        <p>DUBOIS Mickaël</p>        
+                </div>
+        </footer>
+</html>
---
+++ src/vues/connexion.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="../ressources/style/connexion.css">
+
+    <title>Procrastinator Calendar</title>
+  </head>
+  <body>
+      <?php include("../ressources/html_parts/header.php") ?> 
+      <section>
+        <div>
+          <h1>Connexion :</h1>
+          <div>
+            <p>Pseudo :</p>
+            <textarea></textarea>
+          </div>
+          <div>
+            <p>Mot de passe :</p>
+            <textarea></textarea>
+            <a href="#">Mot de passe oublié ?</a>
+          </div>
+          <div>
+              <button>Connexion</button>
+          </div>
+        </div>
+      </section>
+      <?php include("../ressources/html_parts/footer.php") ?>
+  </body>
+</html>
