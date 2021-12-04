9993975ec698d13061e69016117876eefc8f4c8b
omega2028 <omega2028.yt@gmail.com>
2021-12-04 15:01:32 +0100

    ajout controller, les méthodes de gestion des Utilisateurs sont implémentées

---
+++ src/controlleurs/controlleur.php
@@ -0,0 +1,39 @@
+<?php
+
+	require_once("../DAL/ConnexionBD_classe.php");
+	require_once("../DAL/UtilisateurGateway.php");
+	require_once("../config/validateurs/ValidateurGenerique.php");
+
+	class Controlleur {
+		private $connexionBD;
+		private $utlGW;
+		private $validateur;
+		
+		public function __construct(){
+			$connexionBD = ConnexionBD.getInstance("dbmidubois1","midubois1","achanger");
+			$utlGW = new UtilisateurGateway($connexionBD);
+			$validateur = new ValidateurGenerique();
+		}
+		
+		public getUtilisateur(string $pseudo){
+			if($validateur->validerStr($pseudo)){
+				return $utlGW->trouverUtilisateur("pseudo",$pseudo);
+			}
+			//tab err
+		}
+		
+		public addUtilisateur(string $pseudo, string $email, string $ddn, string $mdp){
+			if (validateur->validerStr($pseudo) && validateur->validerStr($email) && validateur->validerStr($ddn) && validateur->validerStr($mdp)){
+				$utlGW->ajouterUtilisateur(new Utilisateur($pseudo, $email, $ddn, $mdp));
+				return;
+			}
+			//tab err
+		}
+		
+		public delUtilisateur(string $pseudo, string $email, string $ddn, string $mdp){
+			if (validateur->validerStr($pseudo) && validateur->validerStr($email) && validateur->validerStr($ddn) && validateur->validerStr($mdp)){
+				$utlGW->supprimerUtilisateur(new Utilisateur($pseudo, $email, $ddn, $mdp));
+				return;
+			}
+			//tab err
+		}
