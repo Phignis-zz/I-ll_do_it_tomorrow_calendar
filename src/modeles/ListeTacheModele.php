57258ff04eaa9db5b4fc0427b5be73a76ee98f83
omega2028 <omega2028.yt@gmail.com>
2021-12-05 10:45:49 +0100

    liste taches modele + vue liste tache

---
+++ src/modeles/ListeTacheModele.php
@@ -0,0 +1,39 @@
+<?php
+
+	require_once("../DAL/ListeTachesGateway.php");
+	require_once("../modeles/metier/ListeTaches_classe.php");
+
+	class ListeTacheModele{
+
+		private $ltacheGW;
+
+		public function __constructor(ListeTachesGateway $ltacheGW){
+			this->$ltacheGW = $ltacheGW;
+		}
+
+		public function trouverListeTacheParID(string $valeurColonne = "") : array {
+			$results = ltacheGW->trouverListeTache('idListe', $valeurColonne);
+			$array = [];
+			for ($results as $row){
+				$array[] = new ListeTaches($row['idListe'],$row['nomListe'],null)
+			}
+			return $array;
+		}
+
+		public function trouverListeTacheParPseudoUtl(string $valeurColonne = "") : array{
+			$results = ltacheGW->trouverListeTache('pseudo', $valeurColonne);
+			$array = [];
+			for ($results as $row){
+				$array[] = new ListeTaches($row['idListe'],$row['nomListe'],null)
+			}
+			return $array;
+		}
+
+		public function ajouterListeTache(Tache $ajout) : bool {
+            return $ltacheGW->ajouterListeTache($ajout);
+        }
+                
+        public function supprimerListeTache(int $idListeTacheSuppression) : bool {
+            return $ltacheGW->supprimerListeTache($idListeTacheSuppression);
+        }
+	}
--- src/modeles/TacheModele.php
+++ src/modeles/TacheModele.php
@@ -1,6 +1,6 @@
 <?php
 
-        require_once("../DAL/TacheGateway");
+        require_once("../DAL/TacheGateway.php");
         
         class TacheModele {
                 
