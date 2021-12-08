bbdc190ed1215c8f8ab6275c1ed219c9470d86db
Mickael DUBOIS <midubois1@IUTCLINFA2319.iut.local>
2021-12-08 17:49:46 +0100

    debut front controller, avancement sur les vues, controller utilisateur

--- src/vues/connexion.php
+++ src/vues/connexion.php
@@ -11,23 +11,19 @@
   </head>
   <body>
       <?php include("../ressources/html_parts/header.php") ?> 
-      <section>
-        <div>
-          <h1>Connexion :</h1>
-          <div>
-            <p>Pseudo :</p>
-            <textarea></textarea>
-          </div>
-          <div>
-            <p>Mot de passe :</p>
-            <textarea></textarea>
-            <a href="#">Mot de passe oublié ?</a>
-          </div>
-          <div>
-              <button>Connexion</button>
-          </div>
-        </div>
-      </section>
+		<section>
+		<h1>Connexion : </h1>
+		
+		<FORM METHOD="POST" ACTION="../controlleurs/controlleur.php">
+			<label for="pseudo">Pseudo :</label>
+			<INPUT TYPE="TEXT" NAME="pseudo" id="pseudo">
+			<label for="mdp">Mot de passe :</label>
+			<INPUT TYPE="TEXT" NAME="mdp" id="mdp">
+			<INPUT TYPE="HIDDEN" NAME="action" VALUE="connexion">
+			<INPUT TYPE="SUBMIT" VALUE="Connexion">
+		</FORM>
+		</section>
+      
       <?php include("../ressources/html_parts/footer.php") ?>
   </body>
 </html>
---
+++ src/vues/inscription.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="../ressources/style/inscription.css">
+
+    <title>Procrastinator Calendar</title>
+  </head>
+  <body>
+      <?php include("../ressources/html_parts/header.php") ?> 
+		<section>
+		<h1>Inscription : </h1>
+		
+		<FORM METHOD="POST" ACTION="../controlleurs/controlleur.php">
+			<label for="pseudo">Pseudo :</label>
+			<INPUT TYPE="TEXT" NAME="pseudo" id="pseudo">
+			<label for="mdp">Mot de passe :</label>
+			<INPUT TYPE="TEXT" NAME="mdp" id="mdp">
+			<label for="ddn">Date de naissance :</label>
+			<INPUT TYPE="TEXT" NAME="ddn" id="ddn">
+			<label for="email">Email :</label>
+			<INPUT TYPE="TEXT" NAME="email" id="email">
+			<INPUT TYPE="HIDDEN" NAME="action" VALUE="inscription">
+			<INPUT TYPE="SUBMIT" VALUE="Connexion">
+		</FORM>
+		</section>
+      
+      <?php include("../ressources/html_parts/footer.php") ?>
+  </body>
+</html>
