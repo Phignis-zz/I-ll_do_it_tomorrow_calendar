0b0d809d52aa2a7eda930cd064340209d465b575
Phignis <baptiste63.foucras.pro@gmail.com>
2021-12-31 10:26:20 +0100

    ajout des dépendances manquantes, connexion.php include bien les choses maintenant que index se trouve a la racine

--- src/vues/connexion.php
+++ src/vues/connexion.php
@@ -5,12 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    <link rel="stylesheet" href="../ressources/style/connexion.css">
+    <link rel="stylesheet" href="ressources/style/connexion.css">
 
     <title>Procrastinator Calendar</title>
   </head>
   <body>
-    <?php include("../ressources/html_parts/header.php") ?>
+    <?php include("ressources/html_parts/header.php") ?>
     <section>
       <h1>Connexion : </h1>
     
@@ -24,6 +24,6 @@
       </FORM>
 		</section>
     
-    <?php include("../ressources/html_parts/footer.php") ?>
+    <?php include("ressources/html_parts/footer.php") ?>
   </body>
 </html>
