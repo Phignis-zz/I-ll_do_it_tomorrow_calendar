868a808a7ccba73885e0543043343bd8deb72dba
Baptiste <baptiste63.pro@gmail.com>
2021-12-31 10:08:26 +0100

    modification de l'arborescence de index.php, il se trovue a la racine, pour que les css soient pris en compte

--- src/vues/connexion.php
+++ src/vues/connexion.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="fr">
-    <head>
+  <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,20 +10,20 @@
     <title>Procrastinator Calendar</title>
   </head>
   <body>
-      <?php include("../ressources/html_parts/header.php") ?> 
-		<section>
-		<h1>Connexion : </h1>
-		
-		<FORM METHOD="POST" ACTION="../controlleurs/controlleur.php">
-			<label for="pseudo">Pseudo :</label>
-			<INPUT TYPE="TEXT" NAME="pseudo" id="pseudo">
-			<label for="mdp">Mot de passe :</label>
-			<INPUT TYPE="TEXT" NAME="mdp" id="mdp">
-			<INPUT TYPE="HIDDEN" NAME="action" VALUE="connexion">
-			<INPUT TYPE="SUBMIT" VALUE="Connexion">
-		</FORM>
+    <?php include("../ressources/html_parts/header.php") ?>
+    <section>
+      <h1>Connexion : </h1>
+    
+      <FORM METHOD="POST" ACTION="../controlleurs/controlleur.php">
+        <label for="pseudo">Pseudo :</label>
+        <INPUT TYPE="TEXT" NAME="pseudo" id="pseudo">
+			  <label for="mdp">Mot de passe :</label>
+        <INPUT TYPE="TEXT" NAME="mdp" id="mdp">
+        <INPUT TYPE="HIDDEN" NAME="action" VALUE="connexion">
+        <INPUT TYPE="SUBMIT" VALUE="Connexion">
+      </FORM>
 		</section>
-      
-      <?php include("../ressources/html_parts/footer.php") ?>
+    
+    <?php include("../ressources/html_parts/footer.php") ?>
   </body>
 </html>
