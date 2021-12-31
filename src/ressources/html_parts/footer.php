065f4039fffd376b173576c4ee333fddaa1d2f57
Baptiste <baptiste63.pro@gmail.com>
2021-12-31 10:37:51 +0100

    les imports de css ne se font que dans la vue principale

--- src/ressources/html_parts/footer.php
+++ src/ressources/html_parts/footer.php
@@ -3,29 +3,16 @@
         Le but de ce fichier est de fournir un footer au projet "The I'll do it tomorrow Calendar".
         Il n'a pas pour but d'être appelé seul, expliquant l'absance de balises html, head ou body
 -->
-
-<!doctype html>
-<html lang="fr">
-        <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            
-        <link rel="stylesheet" href="../ressources/style/footer.css">
-
-        <title>Procrastinator Calendar</title>
-        </head>
-        <footer>
-                <div>
-                       <img src="../ressources/images/logo.png" alt="o">
-                </div>
-                <div>
-                        <p>- Anonymous -</p>
-                </div>
-                <div id="credits">
-                        <p>By:</p>
-                        <p>FOUCRAS Baptiste</p>
-                        <p>DUBOIS Mickaël</p>        
-                </div>
-        </footer>
-</html>
+<footer>
+        <div>
+                <img src="../ressources/images/logo.png" alt="o">
+        </div>
+        <div>
+                <p>- Anonymous -</p>
+        </div>
+        <div id="credits">
+                <p>By:</p>
+                <p>FOUCRAS Baptiste</p>
+                <p>DUBOIS Mickaël</p>        
+        </div>
+</footer>
--- src/ressources/html_parts/header.php
+++ src/ressources/html_parts/header.php
@@ -1,65 +1,41 @@
-<!doctype html>
-<html lang="fr">
-    <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../ressources/style/bootstrap.min.css">
-    <link rel="stylesheet" href="../ressources/style/index.css">
-
-    <!-- Script nécessaire pour faire fonctionner la navbar (dropdown) -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-
-    <!-- Scripts pas encore utilisé -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-    <title>Procrastinator Calendar</title>
-  </head>
-  <body>
-    <header>
-        <div id="titre">
-          <div id="text_titre">
-            <h1>
-              The
-              <img id="img_titre" src="../ressources/images/logo.png" alt="o">
-              Calendar
-            </h1>
-          </div>
-          <div id="account_buttons">
+<header>
+    <div id="titre">
+      <div id="text_titre">
+        <h1>
+          The
+          <img id="img_titre" src="../ressources/images/logo.png" alt="o">
+          Calendar
+        </h1>
+      </div>
+      <div id="account_buttons">
+        <button>
+          <p>Se Connecter</p>
+        </button>
+        <button>
+          <p>S'Inscrire</p>
+        </button>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        
+        <li>
+          <a href="../../vues/listpb.php">
             <button>
-              <p>Se Connecter</p>
+              <p>Listes de tâches publiques</p>
             </button>
+          </a>
+        </li>
+        <li>
+          <a href="">
             <button>
-              <p>S'Inscrire</p>
+              <p>Listes de tâches privées</p>
             </button>
-          </div>
-        </div>
-        <nav>
-          <ul>
-            
-            <li>
-              <a href="../../vues/listpb.php">
-                <button>
-                  <p>Listes de tâches publiques</p>
-                </button>
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <button>
-                  <p>Listes de tâches privées</p>
-                </button>
-              </a>
-              <button href="">
-                <p>Listes de tâches privées</p>
-              </button>
-            </li>
-          </ul>
-        </nav>
-    </header>
-  </body>
- </html>
-
+          </a>
+          <button href="">
+            <p>Listes de tâches privées</p>
+          </button>
+        </li>
+      </ul>
+    </nav>
+</header>
